b8a933368d9085210d7c34c446b35c3244a838d1
butkovevg <butkovevg@gmail.com>
2022-10-07 15:52:25 +0300

    test8

---
+++ ideal_func.py
@@ -0,0 +1,15 @@
+# импорты должны быть отсортированы и разбиты на общие и локальные
+
+def verb_description(input_string: str="вам", input_list: list=[0,1]) -> dict:
+    """
+    ToDo: Что еще необходимо сделать для функции
+    :param input_string:
+    :param input_list:
+    :return:
+    """
+    print(input_string)
+    print(input_list)
+
+    return dict
+verb_description()
+
