50cf5fe789ce3fe83a35614908d74ed823e5282a
butkovevg <butkovevg@gmail.com>
2022-10-07 13:37:33 +0300

    test2

--- manual_GIT.py
+++ manual_GIT.py
@@ -1,1 +1,25 @@
-git init
+# git init
+# git checkout <file> #- отменить изменения или отменить удаления до последнего коммита
+# git checkout <hash-commit> <file>
+# git diff --stage <file>
+# git reset HEAD <file> - удалить из отслеживаемых
+# git commit --amend -m "" изменить сообщение к последнему коммиту
+git show
+git
+git
+git
+git
+git
+git
+git
+git
+git
+git
+git
+git
+git
+git
+git
+git
+
+
